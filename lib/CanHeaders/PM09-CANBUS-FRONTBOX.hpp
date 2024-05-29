beff7dfa320e07552fe2891e5cffebe58080a763
Adam <Adam_Wasilewski_99@proton.me>
2024-05-29 14:54:08 +0200

    Change header name

--- lib/CanHeaders/PM09-CANBUS-FRONTBOX.hpp
+++ lib/CanHeaders/PM09-CANBUS-FRONTBOX.hpp
@@ -15,27 +15,48 @@ enum struct FboxStates : uint8_t {
 };
 
 struct __attribute__((packed)) DriverInput {
-  uint16_t pedalPosition;   // pedal position form 0 – 1000 where 0 means pedal not pressed
-  uint16_t brakePressureFront;
-  uint16_t brakePressureRear;
-  uint16_t steeringWheelPosition;
+	uint16_t pedalPosition;   // pedal position form 0 – 1000 where 0 means pedal not pressed
+	uint16_t brakePressureFront;
+	uint16_t brakePressureRear;
+	uint16_t steeringWheelPosition;
 };
 
 struct __attribute__((packed)) FrontData {
-  uint8_t sc_state;   // pedal position form 0 – 1000 where 0 means pedal not pressed
-  uint16_t frontLeftSuspension;
-  uint16_t frontRightSuspension;
-  uint8_t frontLeftHubTemperature;
-  uint8_t frontRightHubTemperature;
+	  bool sense_left_kill: 1;
+	  bool sense_right_kill: 1;
+	  bool sense_driver_kill: 1;
+	  bool sense_inertia: 1;
+	  bool sense_bspd: 1;
+	  bool sense_overtravel: 1;
+	  bool sense_right_wheel: 1;
+	  uint16_t frontLeftSuspension;
+	  uint16_t frontRightSuspension;
+	  uint8_t frontLeftHubTemperature;
+	  uint8_t frontRightHubTemperature;
 };
 
 const uint8_t DRIVER_INPUT_CAN_DLC = sizeof(DriverInput);
 const uint8_t DRIVER_INPUT_FREQUENCY = 100;
 
+const uint8_t FRONT_DATA_CAN_DLC = sizeof(FrontData);
+const uint8_t FRONT_DATA_FREQUENCY = 10;
+
 #ifndef PUTM_USE_CAN_FD
 
 const FDCAN_TxHeaderTypeDef can_tx_header_DRIVER_INPUT{
 	DRIVER_INPUT_CAN_ID,
+	FDCAN_STANDARD_ID,
+	FDCAN_DATA_FRAME,
+	DRIVER_INPUT_CAN_DLC,
+	FDCAN_ESI_PASSIVE,
+	FDCAN_BRS_OFF,
+	FDCAN_CLASSIC_CAN,
+	FDCAN_NO_TX_EVENTS,
+	0
+};
+
+const FDCAN_TxHeaderTypeDef can_tx_header_FRONT_DATA{
+	FRONT_DATA_CAN_ID,
 	FDCAN_STANDARD_ID,
 	FDCAN_DATA_FRAME,
 	DRIVER_INPUT_CAN_DLC,
