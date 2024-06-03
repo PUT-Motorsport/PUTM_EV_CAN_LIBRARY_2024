a374b99b12a367918ca566e75ce8f45db6153f87
Adam <Adam_Wasilewski_99@proton.me>
2024-06-03 13:04:44 +0200

    Change can ids of current frames

--- lib/CanHeaders/can_ids.hpp
+++ lib/CanHeaders/can_ids.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <cstdint>
 
-const uint16_t DRIVER_INPUT_CAN_ID = 0x5;
-const uint16_t FRONT_DATA_CAN_ID = 0x66;
-const uint16_t REARBOX_MAIN_CAN_ID = 0x32;
-const uint16_t DASHBOARD_MAIN_CAN_ID = 0x3C;
-
-
+/* Primary Can Bus */
+const uint16_t DRIVER_INPUT_CAN_ID   = 0x05;
+const uint16_t DASHBOARD_MAIN_CAN_ID = 0x15;
+const uint16_t REARBOX_MAIN_CAN_ID   = 0x25;
+const uint16_t FRONT_DATA_CAN_ID     = 0x35;
+/* Secondary Can Bus */
 constexpr uint16_t FRONT_LEFT_AMK_ACTUAL_VALUES_1_CAN_ID = 0x282 + 1;  // + n
 constexpr uint16_t FRONT_LEFT_AMK_ACTUAL_VALUES_2_CAN_ID = 0x284 + 1;  // + n
 constexpr uint16_t FRONT_LEFT_AMK_SETPOINTS_1_CAN_ID = 0x183 + 1;  // + n
