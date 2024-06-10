8d07d37c13793ef209f4f2681eb70a1ebc32fc19
Adam <Adam_Wasilewski_99@proton.me>
2024-06-10 19:49:05 +0200

    Fix PC header file

--- lib/CanHeaders/PM09_CANBUS_PC.hpp
+++ lib/CanHeaders/PM09_CANBUS_PC.hpp
@@ -20,14 +20,14 @@ struct __attribute__((packed)) PcMainData {
     bool inverters_ready : 1;
 };
 
-const uint8_t PC_MAIN_CAN_DLC = sizeof(DriverInput);
+const uint8_t PC_MAIN_CAN_DLC = sizeof(PcMainData);
 const uint8_t PC_MAIN_FREQUENCY = 50;
 
 const FDCAN_TxHeaderTypeDef can_tx_header_PC_MAIN_DATA {
-        DRIVER_INPUT_CAN_ID,
+		PC_MAIN_CAN_ID,
         FDCAN_STANDARD_ID,
         FDCAN_DATA_FRAME,
-        DRIVER_INPUT_CAN_DLC,
+		PC_MAIN_CAN_DLC,
         FDCAN_ESI_PASSIVE,
         FDCAN_BRS_OFF,
         FDCAN_CLASSIC_CAN,
