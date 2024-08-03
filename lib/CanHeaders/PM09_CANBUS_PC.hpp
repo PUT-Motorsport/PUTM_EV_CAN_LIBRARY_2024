9832a2d59c5971c5f2644f2ba6445d0ee04c7711
KozAAAAA <mateusz-koza@outlook.com>
2024-08-03 21:03:12 +0200

    Update pcmaindata can message

--- lib/CanHeaders/PM09_CANBUS_PC.hpp
+++ lib/CanHeaders/PM09_CANBUS_PC.hpp
@@ -4,20 +4,7 @@
 namespace PUTM_CAN {
 
 struct __attribute__((packed)) PcMainData {
-    uint8_t vehicleSpeed;
-    /* Inverter temperatures */
-    uint8_t frontLeftInverterTemperature : 6;
-    uint8_t frontRightInverterTemperature : 6;
-    uint8_t rearLeftIverterTemperature : 6;
-    uint8_t rearRightIverterTemperature : 6;
-    /* Motor temperatures */
-    uint8_t frontLeftMotorTemperature : 6;
-    uint8_t frontRightMotorTemperature : 6;
-    uint8_t rearLeftMotorTemperature : 6;
-    uint8_t rearRightMotorTemperature : 6;
-
-    bool rtd : 1;
-    bool inverters_ready : 1;
+    uint32_t time;
 };
 
 const uint8_t PC_MAIN_CAN_DLC = sizeof(PcMainData);
