de0a5d286aa5159e0841dd4084dedf07f5b1b5c6
Michał Błotniak <michalblotniak@gmail.com>
2025-04-05 10:51:39 +0200

    Changing data sizes in PC frame

--- lib/CanHeaders/PM09_CANBUS_PC.hpp
+++ lib/CanHeaders/PM09_CANBUS_PC.hpp
@@ -4,14 +4,14 @@
 namespace PUTM_CAN {
 
 struct __attribute__((packed)) PcTemperatureData {
-    uint8_t rearRightInverterTemperature : 7;  // Range 20-100
-    uint8_t rearLeftInverterTemperature : 7;   // Range 20-100
-    uint8_t rearRightMotorTemperature : 7;     // Range 20-130
-    uint8_t rearLeftMotorTemperature : 7;      // Range 20-130
-    uint8_t frontRightInverterTemperature : 7; // Range 20-100
-    uint8_t frontLeftInverterTemperature : 7;  // Range 20-100
-    uint8_t frontRightMotorTemperature : 7;    // Range 20-130
-    uint8_t frontLeftMotorTemperature : 7;     // Range 20-130
+    uint8_t rearRightInverterTemperature;  // Range 20-100
+    uint8_t rearLeftInverterTemperature;   // Range 20-100
+    uint8_t rearRightMotorTemperature;     // Range 20-130
+    uint8_t rearLeftMotorTemperature;      // Range 20-130
+    uint8_t frontRightInverterTemperature; // Range 20-100
+    uint8_t frontLeftInverterTemperature;  // Range 20-100
+    uint8_t frontRightMotorTemperature;    // Range 20-130
+    uint8_t frontLeftMotorTemperature;     // Range 20-130
 };
 
 const uint8_t PC_TEMPERATURE_CAN_DLC = sizeof(PcTemperatureData);
@@ -21,9 +21,8 @@ const FDCAN_TxHeaderTypeDef can_tx_header_PC_TEMPERATURE_DATA{
     PC_TEMPERATURE_CAN_ID, FDCAN_STANDARD_ID, FDCAN_DATA_FRAME, PC_TEMPERATURE_CAN_DLC, FDCAN_ESI_PASSIVE, FDCAN_BRS_OFF, FDCAN_CLASSIC_CAN, FDCAN_NO_TX_EVENTS, 0};
 
 struct __attribute__((packed)) PcMainData {
-    uint8_t vehicleSpeed : 6;                  
-    uint8_t power : 6;                         
-    uint16_t rpm : 15;                         
+    uint8_t vehicleSpeed : 16;                  
+    uint8_t torque_current : 16;                                               
     bool invertersReady : 1;                   // 1 bit
     bool rtd : 1;                              // 1 bit
     bool inv_FL_status : 1;                    // Status, 1 bit
